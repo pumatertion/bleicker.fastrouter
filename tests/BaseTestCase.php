28c2ba62690dc5f9041a49de31fe81583b70c8de
Carsten Bleicker <carsten@bleicker.de>
2015-05-22 13:18:10 +0200

    Refactoring

---
+++ tests/BaseTestCase.php
@@ -0,0 +1,11 @@
+<?php
+namespace Tests\Bleicker\FastRouter;
+
+/**
+ * Class BaseTestCase
+ *
+ * @package Tests\Bleicker\FastRouter
+ */
+abstract class BaseTestCase extends \PHPUnit_Framework_TestCase {
+
+}
